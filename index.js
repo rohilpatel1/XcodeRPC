f2fbff14346e84168ca624b404b9d6e33700f59c
Rohil Patel <8158301@isd535.org>
2020-09-13 16:30:23 +0000

    adding support for electron

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const applescript = require('applescript');
 const fs = require('fs');
-const client = require('discord-rich-presence')(754460701522788550)
+const client = require('discord-rich-presence')('754460701522788550');
+const { app, BrowserWindow } = require('electron');
 
 let system = 'tell application "System Events" to (name of processes) contains "Xcode"';
 let getextension = 'tell application "Xcode" to get the name of the front window' 
@@ -12,6 +13,27 @@ let config = {
   "showDetails": true,
   "showState": true
 }
+
+let startTime = Date.now();
+
+/*
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+*/
+
 
 function update() {
 	applescript.execString(system, (err, res) => {
@@ -26,7 +48,7 @@ function update() {
 		    let workspace, fileExtension;
 		    
 		    if (project) workspace = project.replace('.xcodeproj', '');
-		    if (res) fileExtension = res.match(/\.(.+)/g) ? res.match(/\.(.+)/g)[0] : 'Unknown';
+		    if (res) fileExtension = res.match(/\.(.+)/g) ? res.match(/\.(.+)/g)[0] : 'unknown';
 		    
         var state = config.state.replace(/{file}/g, res);
         var details = config.details.replace(/{project}/g, workspace);
@@ -44,8 +66,8 @@ function update() {
 		  });
 		});
 	});
-}
-update()
+};
+update();
 
 setInterval(() => {
   update()
