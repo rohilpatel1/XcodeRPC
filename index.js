863a76b17284f0d2fe4d19c653d21511b4d45c4f
Rohil Patel <8158301@isd535.org>
2020-10-25 02:25:56 +0000

    Made use of the applescript file

--- index.js
+++ index.js
@@ -1,84 +1,74 @@
-const { execString } = require('applescript');
-const { updatePresence } = require('discord-rich-presence')('754460701522788550');
+const applescript = require('applescript');
+const Path = require('path');
+const fs = require('fs');
+const client = require('discord-rich-presence')('754460701522788550');
 const { app, BrowserWindow } = require('electron');
 
-let scpt = {
-	system:
-		'tell application "System Events" to (name of processes) contains "Xcode"',
-	getextension: 'tell application "Xcode" to get the name of the front window',
-	getworkspace:
-		'tell application "Xcode" to get the name of the active workspace document'
-};
+let readData = fs.readFileSync(Path.join(__dirname, '.', 'main.applescript'), 'utf8');
 
-let { system, getextension, getworkspace } = scpt;
+console.log(Path.join(__dirname, 'main.applescript'));
+let linesArr = readData.split(/\r?\n/);
+
 
 let config = {
-	details: 'Workspace: {project}',
-	state: 'Editing {file}',
-	showDetails: true,
-	showState: true
-};
+  "details": "Workspace: {project}",
+  "state": "Editing {file}",
+  "showDetails": true,
+  "showState": true
+}
 
 let startTime = Date.now();
 
-function createWindow() {
-	const win = new BrowserWindow({
-		width: 0,
-		height: 0,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	});
-	win.loadFile('index.html');
-}
-app.whenReady().then(createWindow);
 
-function update() {
-	execString(system, (err, res) => {
-		if (!res) {
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 0,
+    height: 0,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+app.whenReady().then(createWindow)
+
+
+
+function update() { //system
+	applescript.execString(linesArr[0], (err, res) => {
+	  
+		if (res == false) {
 			console.log('Xcode not open');
 			return;
 		}
+		
+		applescript.execString(linesArr[1], (err, res) => { //getextension
+		  applescript.execString(linesArr[2], (err, project) => {//getworkspace
+		    let workspace, fileExtension;
+		    
+		    if (project) workspace = project.replace('.xcodeproj', '');
+		    if (res) fileExtension = res.match(/\.(.+)/g) ? res.match(/\.(.+)/g)[0] : 'unknown';
+		    
+        var state = config.state.replace(/{file}/g, res);
+        var details = config.details.replace(/{project}/g, workspace);
 
-		execString(getextension, (err, res) => {
-			execString(getworkspace, (err, project) => {
-				let workspace, fileExtension;
 
-				if (project) workspace = project.replace('.xcodeproj', '');
-				if (res)
-					fileExtension = res.match(/\.(.+)/g)
-						? res.match(/\.(.+)/g)[0]
-						: 'unknown';
-
-				let state = config.state.replace(/{file}/g, res);
-				let details = config.details.replace(/{project}/g, workspace);
-
-				updatePresence({
-					state: config.showState ? (!res ? undefined : state) : undefined,
-					details: config.showDetails
-						? !workspace
-							? 'Idling'
-							: details
-						: undefined,
-					startTimestamp: !res ? undefined : startTime,
-					largeImageKey: 'xcode',
-					largeImageText: 'Editing in Xcode',
-					smallImageKey: fileExtension
-						? fileExtension === '.swift'
-							? 'swift'
-							: fileExtension === '.plist'
-								? 'plist'
-								: 'unknown'
-						: undefined,
-					smallImageText: fileExtension
-						? `Editing a ${fileExtension} file`
-						: undefined,
-					instance: true
-				});
-			});
+        client.updatePresence({
+          state: config.showState ? !res ? undefined : state : undefined,
+          details: config.showDetails ? !workspace ? 'Idling' : details : undefined,
+          startTimestamp: !res ? undefined : startTime,
+          largeImageKey: 'xcode',
+          largeImageText: 'Editing in Xcode',
+          smallImageKey: fileExtension ? fileExtension === '.swift' ? 'swift' : fileExtension === '.plist' ? 'plist' : 'unknown' : undefined,
+          smallImageText: fileExtension ? `Editing a ${fileExtension} file` : undefined,
+          instance: true,
+        });
+		  });
 		});
 	});
-}
+};
 update();
 
-setInterval(update, 5000);
+setInterval(update, 1000);
